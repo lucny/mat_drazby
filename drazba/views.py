5872aced093327335b0dacff76254ee6515e8a87
lucny <lucny@sspu-opava.cz>
2022-04-17 20:42:26 +0200

    04 - uvodni strana

--- drazba/views.py
+++ drazba/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Predmet
+
+
+def index(request):
+    context = {
+        'mesic': 'květen 2022',
+        'drazby': Predmet.objects.filter(zacatek_drazby__month=5, zacatek_drazby__year=2022).order_by('zacatek_drazby')[:4]
+    }
+    return render(request, template_name='index.html', context=context)
--- mat_drazba/urls.py
+++ mat_drazba/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('drazba/', include('drazba.urls')),
+    path('', RedirectView.as_view(url='drazba/'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
